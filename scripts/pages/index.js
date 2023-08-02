178abb24f0777694804bcdcbc476bc10365a2849
Julie Souvandy <julie.souvandy.pro@gmail.com>
2023-08-02 08:12:09 +0200

    Différentes étapes - ajout des images de profil des photographes et stylisation de celles ci - mise en place du fetch pour récupérer les données sur pg accueil - mise en place du factory photographer.js - mise en place du template photographer.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,11 @@
     async function getPhotographers() {
+        const response = await fetch('/data/photographers.json');
+        const photographers = await response.json();
+        return photographers;
+
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
+        /*let photographers = [
             {
                 "name": "Ma data test",
                 "id": 1,
@@ -23,8 +27,11 @@
         ]
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers, ...photographers, ...photographers]})*/
+            
     }
+
+
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,37 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+
         const article = document.createElement( 'article' );
+
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        
+        const spanCity = document.createElement ( 'span' );
+        spanCity.textContent = city;
+
+        const spanCountry = document.createElement ( 'span' );
+        spanCountry.textContent = country;
+
+        const spanTagline = document.createElement ( 'span' );
+        spanTagline.textContent = tagline;
+
+        const spanPrice = document.createElement ( 'span' );
+        spanPrice.textContent = price ;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(spanCity);
+        article.appendChild(spanCountry); 
+        article.appendChild(spanTagline);
+        article.appendChild(spanPrice);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM };
 }
