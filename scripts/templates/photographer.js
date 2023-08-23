515438bb935e2d84d88b4ecc4ac64674248adc8e
Julie Souvandy <julie.souvandy.pro@gmail.com>
2023-08-23 08:21:03 +0200

    etape 3 : factory function + accessibilite aria-label et alt + layouts flexbox et css grids

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,11 +7,21 @@ function photographerTemplate(data) {
 
         const article = document.createElement( 'article' );
 
+        const link = document.createElement('a');
+        link.setAttribute('href', 'photographer.html?id='+ id);
+        link.setAttribute('title', 'Lien vers la fiche de ' + name);
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture);
+        img.setAttribute("alt", "Photo de " + name);
+        img.setAttribute("role", "img");
+        link.appendChild(img);
+        
 
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        h2.setAttribute("focusable", true);
+        link.appendChild(h2);
         
         const spanCity = document.createElement ( 'span' );
         spanCity.textContent = city;
@@ -23,10 +33,9 @@ function photographerTemplate(data) {
         spanTagline.textContent = tagline;
 
         const spanPrice = document.createElement ( 'span' );
-        spanPrice.textContent = price ;
+        spanPrice.textContent = price +"€/jour";
 
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(link);
         article.appendChild(spanCity);
         article.appendChild(spanCountry); 
         article.appendChild(spanTagline);
